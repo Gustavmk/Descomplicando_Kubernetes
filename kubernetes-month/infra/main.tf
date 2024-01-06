37957ca68f095281aa049464b7809f031790a881
gustavo.kuno <gustavo.mkuno@gmail.com>
2024-01-06 16:52:05 -0300

    ignore pem file

--- kubernetes-month/infra/main.tf
+++ kubernetes-month/infra/main.tf
@@ -1,36 +1,19 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "drylabs-vpc"
-  cidr = local.vpc_cidr
-
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, 2)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, 4)]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = false
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+# Create RSA key of size 4096 bits
+resource "tls_private_key" "tf_ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
+# Create local file
+resource "local_file" "tf_ec2_key" {
+  content  = tls_private_key.tf_ec2_key.private_key_pem
+  filename = "${path.module}/tf_ec2_key.pem"
+}
 
-/* 
-# Create key pair
-aws ec2 create-key-pair \
-    --key-name deployer-key \
-    --key-type rsa \
-    --key-format pem \
-    --query "KeyMaterial" \
-    --output text > deployer-key.pem
-*/
-
-data "aws_key_pair" "deployer" {
-  key_name           = "deployer-key"
-  include_public_key = true
+# Create AWS key pair
+resource "aws_key_pair" "tf_ec2_key" {
+  key_name   = "tf_ec2_key"
+  public_key = tls_private_key.tf_ec2_key.public_key_openssh
 }
 
 module "ec2_instance" {
@@ -40,12 +23,13 @@ module "ec2_instance" {
   name  = "${local.name}-k8s-${count.index}"
 
   instance_type = "t2.medium"
-  key_name      = data.aws_key_pair.deployer.key_name
+  key_name      = aws_key_pair.tf_ec2_key.key_name
   monitoring    = false
 
   vpc_security_group_ids      = [module.vpc.default_security_group_id]
   subnet_id                   = module.vpc.private_subnets[0]
   associate_public_ip_address = true
+
 
   tags = {
     Terraform   = "true"
